9109e97743673b79c027db8dc76542faaa48e9e3
dKelvine1548 <71010229+dKelvine1548@users.noreply.github.com>
2020-10-30 11:19:37 +0100

    copie des nouveaux fichiers

---
+++ Calculatrice/js/script.js
@@ -0,0 +1,28 @@
+console.log("Hello world!");
+function value(valeur){
+    console.log(valeur);
+var button = document.querySelectorAll(".button");
+console.log(button);
+}
+
+function addition(nombre1 ,nombre2){
+   var result= nombre1 + nombre2;
+   return result;
+}
+
+function soustraction(nombre1 ,nombre2){
+    var result= nombre1 - nombre2;
+    return result;
+ }
+
+ function multiplication(nombre1 ,nombre2){
+    var result= nombre1 * nombre2;
+    return result;
+ }
+ 
+ function addition(nombre1 ,nombre2){
+    var result= nombre1 / nombre2;
+    return result;
+ }
+ 
+ 
